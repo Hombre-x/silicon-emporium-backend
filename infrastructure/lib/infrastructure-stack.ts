da9168289ea7c60e1e1461c0c73129be58e7b68d
Hombre-x <gabisanpar@gmail.com>
2022-11-25 12:14:44 -0500

    Added aws infrastructure for future implementations.

---
+++ infrastructure/lib/infrastructure-stack.ts
@@ -0,0 +1,26 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class InfrastructureStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new ec2.Vpc(this, 'ApiVpc', {
+      subnetConfiguration: [
+        { name: 'ingress', subnetType: ec2.SubnetType.PUBLIC },
+        { name: 'isolated', subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+      ]
+    });
+
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
