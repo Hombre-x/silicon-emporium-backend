3ab45a23d68c285b142cc346ca04fac8c6b01d72
Hombre-x <gabisanpar@gmail.com>
2023-08-05 22:15:59 -0500

    Added a custom Show for AppConfig to improve legibility on console

--- src/main/scala/dev/firework/app/Main.scala
+++ src/main/scala/dev/firework/app/Main.scala
@@ -1,11 +1,7 @@
 package dev.firework.app
 
-import cats.Monad
-import cats.syntax.all.*
-import cats.effect.{ExitCode, IO, IOApp, Resource}
-
-import org.typelevel.log4cats.{Logger, LoggerFactory, LoggerName, SelfAwareStructuredLogger}
-import org.typelevel.log4cats.slf4j.{Slf4jFactory, Slf4jLogger, loggerFactoryforSync}
+import cats.syntax.show.*
+import cats.effect.{ExitCode, IO, IOApp}
 
 import com.comcast.ip4s.*
 
@@ -19,14 +15,15 @@ import dev.firework.http.HttpApi
 import dev.firework.amenities.*
 import dev.firework.domain.config.AppConfig
 import dev.firework.config.Config
+import dev.firework.instances.ConfigInstances.given
 
 object Main extends IOApp:
 
   override def run(args: List[String]): IO[ExitCode] = Config
     .load[IO]
-    .flatMap(cfg =>
-      logger.info(s"Loading configurations: $cfg") >>
-        AppResources.make[IO](cfg).use { resources =>
+    .flatMap: cfg =>
+      logger.info(s"Loading configurations: ${cfg.show}") >>
+        AppResources.make[IO](cfg).use: resources =>
 
           val clients  = AppClients.make(resources.client)
           val program  = AppPrograms.make(clients)
@@ -37,9 +34,6 @@ object Main extends IOApp:
           server.useForever
             .onCancel(logger.info("Closing server..."))
             .as(ExitCode.Success)
-
-        }
-    )
 
   end run
   given logger: Logger[IO] = Slf4jLogger.getLoggerFromName("Main")
